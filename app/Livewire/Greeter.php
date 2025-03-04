a3f7fa088678f850780f54e32f69d5c6853d4aa4
Shahzad Ali <shahzad.ali@manafatech.com>
2025-03-04 07:08:42 +0500

    usage of select

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -6,10 +6,10 @@ use Livewire\Component;
 
 class Greeter extends Component
 {
-    public $name = 'Shahzad';
+    public $name = '';
+    public $greeting = '';
 
-    public function changeName($newName) {
-        $this->name = $newName;
+    public function changeName() {
     }
     public function render()
     {
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,15 +1,23 @@
 <div>
     <div>
         <h1>Care about people's approval and you will be their prisoner.</h1>
-        <h1>Hi, {{ $name}}</h1>
+        @if($name)
+            <h1>{{ $greeting}}, {{ $name}}</h1>
+        @endif
     </div>
-    <form wire:submit="changeName(document.querySelector('#changeName').value)">
+    <form wire:submit="changeName()">
         <div class="mt-6">
-            <input type="text" class="block w-full p-4 rounded-md bg-black text-white" id="changeName">
+        <select type="text" class="p-4 border rounded-md bg-gray-700 text-white" wire:model.fill="greeting">
+
+            <option value="salam">Salam</option>
+            <option value="Hi">Hi</option>
+            <option value="Hey">Hey</option>
+            <option value="Howdy" selected>Howdy</option>
+        </select>
+            <input type="text" wire:model="name" class="p-4 border rounded-md bg-gray-700 text-white" id="changeName">
         </div>
         <div class="mt-6">
-            <button class="text-white font-medium rounded -md px-4 py-2 bg-blue-500"
-            type="submit">
+            <button class="text-white font-medium rounded -md px-4 py-2 bg-blue-500" type="submit">
                 Greet
             </button>
         </div>
