74b4630f9ed90bab32b2c9d986613d590f7b293c
Shahzad Ali <shahzad.ali@manafatech.com>
2025-03-05 06:31:07 +0500

    validations using rules and attributes

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -3,14 +3,28 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 
 class Greeter extends Component
 {
+    #[Validate('required|min:6')]
     public $name = '';
     public $greeting = '';
+    public $greetingMessage = '';
 
-    public function changeName() {
+    public function changeGreeting() {
+        $this->reset('greetingMessage');
+
+        $this->validate();
+        $this->greetingMessage = "{$this->greeting}, {$this->name}!";
     }
+
+    // public function rules()
+    // {
+    //     return [
+    //         'name' => 'required|min:6'
+    //     ];
+    // }
     public function render()
     {
         return view('livewire.greeter');
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,11 +1,11 @@
 <div>
     <div>
         <h1>Care about people's approval and you will be their prisoner.</h1>
-        @if($name)
-            <h1>{{ $greeting}}, {{ $name}}</h1>
+        @if($greetingMessage)
+            <h1>{{ $greetingMessage}}</h1>
         @endif
     </div>
-    <form wire:submit="changeName()">
+    <form wire:submit="changeGreeting()">
         <div class="mt-6">
         <select type="text" class="p-4 border rounded-md bg-gray-700 text-white" wire:model.fill="greeting">
 
@@ -15,6 +15,11 @@
             <option value="Howdy" selected>Howdy</option>
         </select>
             <input type="text" wire:model="name" class="p-4 border rounded-md bg-gray-700 text-white" id="changeName">
+        </div>
+        <div>
+            @error('name')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
         </div>
         <div class="mt-6">
             <button class="text-white font-medium rounded -md px-4 py-2 bg-blue-500" type="submit">
